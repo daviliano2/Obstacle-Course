f8cfbe1976746f4ce012fd1cd0557c00ba4f653a
daviliano2 <daviliano2@gmail.com>
2021-02-24 20:53:31 +0100

    videos 14-15
    Make movement frame independent with `Time.deltaTime` and a speed variable.
    Add a cinemachine component to the main camera and a virtual camera object to follow the player object

--- Assets/Scenes/Scripts/Mover.cs
+++ Assets/Scenes/Scripts/Mover.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-
+    [SerializeField] float movementSpeed = 2f;
+    
     // Start is called before the first frame update
     void Start()
     { }
@@ -12,8 +13,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
+        float xValue = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
         transform.Translate(xValue, 0f, zValue);
     }
 }
