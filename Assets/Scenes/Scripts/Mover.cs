baebd33c11b1f48eda607b966643793c5de9a964
daviliano2 <daviliano2@gmail.com>
2021-03-01 18:57:55 +0100

    videos 17-19.
    Add OnCollisionEnter() to walls.
    Small refactor in mover script

--- Assets/Scenes/Scripts/Mover.cs
+++ Assets/Scenes/Scripts/Mover.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 2f;
-    
+
     // Start is called before the first frame update
     void Start()
     { }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
