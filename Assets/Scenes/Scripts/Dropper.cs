2b575a31401329bdf847dc4652e02bce62504090
daviliano2 <daviliano2@gmail.com>
2021-03-04 20:35:34 +0100

    videos 23-24
    Modify meshRenderer and rigidbody properties at start and after 3 seconds have past

--- Assets/Scenes/Scripts/Dropper.cs
+++ Assets/Scenes/Scripts/Dropper.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    [SerializeField] float timeToWait = 3f;
+
+    MeshRenderer obstacleRenderer;
+    Rigidbody obstacleRigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        obstacleRenderer = GetComponent<MeshRenderer>();
+        obstacleRigidbody = GetComponent<Rigidbody>();
+
+        obstacleRenderer.enabled = false;
+        obstacleRigidbody.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Time.time);
+        if (Time.time > timeToWait)
+        {
+            Debug.Log("3 seconds have past");
+            obstacleRenderer.enabled = true;
+            obstacleRigidbody.useGravity = true;
+        }
     }
 }
